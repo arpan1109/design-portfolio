8ce99c59ffdae6528fbb8ef55b0e021f01f642b1
Arpan <palarpan11092004@gmail.com>
2026-05-19 14:07:35 +0530

    changes in hero UI

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,28 +1,35 @@
 export default function Hero() {
   return (
-    <section id="home" className="relative h-screen bg-black text-white flex flex-col items-center justify-center overflow-hidden">
-
+    <section
+      id="home"
+      className="relative h-[50vh] bg-black text-white flex flex-col items-center justify-center overflow-hidden"
+    >
       {/* Massive Typography - NOW USING PRATA */}
       <div className="text-center z-10 px-4">
-        <h1 className="text-7xl md:text-9xl lg:text-[11rem] font-prata leading-none tracking-tight">
-          Crafting 
+        <h1 className="text-4xl md:text-4xl lg:text-5xl font-prata leading-none tracking-tight">
+          Crafting
         </h1>
-        <h1 className="text-4xl md:text-6xl lg:text-[11rem] font-prata italic leading-none text-zinc-500 pr-4 md:pr-12">
+        <h1 className="text-4xl md:text-3xl lg:text-4xl font-prata italic leading-none text-zinc-500 pr-4 md:pr-12">
           Instant clicks
         </h1>
+        <p className="text-3xl md:text-2xl lg:text-2xl font-onest  leading-none text-white pr-4 md:pr-12">
+          Elevate your content with strategic designs built to dominate the
+          feed.
+        </p>
       </div>
 
       {/* Bottom Explore Button & Scroll Line */}
-      <div className="absolute bottom-1 flex flex-col items-center gap-6">
+      <div className="absolute bottom-1 flex flex-row items-center gap-6">
         <button className="border border-zinc-600 rounded-sm font-onest font-bold hover:border-white hover:bg-white/30 transition-all duration-300 px-8 py-3 text-xs tracking-[0.2em] uppercase">
-        <a href="#work">
-          Explore Work
-        </a>
+          <a href="#work">Get Started</a>
         </button>
-        
+        <button className="border border-zinc-600 rounded-sm font-onest font-bold hover:border-white hover:bg-white/30 transition-all duration-300 px-8 py-3 text-xs tracking-[0.2em] uppercase">
+          <a href="#work">Explore Work</a>
+        </button>
+
         {/* Fading Scroll Line */}
-        <div className="w-px h-18 bg-linear-to-b from-zinc-500 to-transparent"></div>
       </div>
+      <div className="w-px h-18 bg-linear-to-b from-zinc-500 to-transparent"></div>
     </section>
   );
 }
